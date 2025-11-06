49f5306dae601232d8e92b17ce42c4460060ae9d
Javieroliver-web <javip200555@gmail.com>
2025-11-06 14:42:30 +0100

    Mejoras

--- projects/shared/src/lib/foto.interface.ts
+++ projects/shared/src/lib/foto.interface.ts
@@ -1,8 +1,7 @@
-// projects/shared/src/lib/interfaces/foto.interface.ts
-export interface Foto { // [cite: 28]
-    albumId: number; // [cite: 30]
-    id: number; // [cite: 31]
-    title: string; // [cite: 32]
-    url: string; // [cite: 33]
-    thumbnailUrl: string; // [cite: 34]
+export interface Foto { 
+    albumId: number; 
+    id: number; 
+    title: string; 
+    url: string; 
+    thumbnailUrl: string; 
   }
